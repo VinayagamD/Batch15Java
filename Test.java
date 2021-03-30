2a1bc6fe83313e2ec2733f61a01461adfb33b121
VinayagamD <vinayagam.d.ganesh@gmail.com>
2021-03-30 20:12:32 +0530

    Initial code commit

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+
+   public static void main(String[] args) {
+       System.out.println("Hi Natasha, Welcome Training");
+       for (String arg: args){
+           System.out.println(arg);
+       }
+   }
+}
